2b1d365b01d1d6b8885c6d7eff43a95913eec8b3
AndriyFartukh <fartuhandriy2003@gmail.com>
2024-01-19 16:18:36 +0200

    HomeWork8

---
+++ HomeWork8/HomeWork8/Models/Sweet.cs
@@ -0,0 +1,21 @@
+﻿namespace HomeWork8.Models
+{
+    public class Sweet : ISweet
+    {
+        public string Name { get; set; }
+
+        public int Weight { get; set; }
+
+        public Sweet(string name, int weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+
+        public virtual string Display()
+        {
+            return $"Name Sweet:{Name}, Weight:{Weight} grams";
+        }
+    }
+}
