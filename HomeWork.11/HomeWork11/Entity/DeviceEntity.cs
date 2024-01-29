125e8c201244510fed173afa058fc51ebef7efc3
AndriyFartukh <fartuhandriy2003@gmail.com>
2024-01-29 21:41:26 +0200

    h

--- HomeWork.11/HomeWork11/Entity/DeviceEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HomeWork11.Entity
-{
-    public class DeviceEntity
-    {
-        public string Name { get; set; }
-        public int EnergyUsage { get; set; }
-    }
-}
--- HomeWork11/HomeWork11/Repositories/Abstractions/IElectronicDeviceRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HomeWork11.Repositories.Abstractions
-{
-    using HomeWork11.Services.Abstraction;
-
-    public interface IElectronicDeviceRepository
-    {
-        IElectronicDeviceService[] CreatedElectronicDevices();
-    }
-}
