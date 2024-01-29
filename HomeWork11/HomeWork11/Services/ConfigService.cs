0bcabf1510533423165ab505cc409a798c909a6b
AndriyFartukh <fartuhandriy2003@gmail.com>
2024-01-29 21:37:50 +0200

    homework

---
+++ HomeWork11/HomeWork11/Services/ConfigService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork11.Services
+{
+    public class ConfigService
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetLoggingLevel()
+        {
+            return _configuration["Logging:Default"];
+        }
+    }
+}
