de78d1c5e8f4c51c0f44a71a3018a6d1582bdf9c
AndriyFartukh <fartuhandriy2003@gmail.com>
2024-02-21 14:20:47 +0200

    HomeWork16

---
+++ HomeWork16/HomeWork16/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeWork16
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            TextReader textReader = new TextReader();
+            string concatenatedText = await textReader.ConcatenateTextAsync();
+
+            Console.WriteLine(concatenatedText);
+        }
+
+    }
+}
