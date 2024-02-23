7119fba5c950a8c1e1ddbf3bcec510ae022c1470
AndriyFartukh <fartuhandriy2003@gmail.com>
2024-02-23 18:18:57 +0200

    HomeWork17

---
+++ HomeWork17/HomeWork17/MessageBox/WindowStateEventArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeWork17.MessageBox
+{
+    public class WindowStateEventArgs
+    {
+        public State WindowState { get; set; } 
+
+        public WindowStateEventArgs(State windowState) 
+        {
+            WindowState = windowState;
+        }
+
+    }
+}
