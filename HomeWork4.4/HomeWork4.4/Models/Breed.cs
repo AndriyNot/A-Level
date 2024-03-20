b656ab6b94e0199b64154a5eecec8c53054cc14e
AndriyFartukh <fartuhandriy2003@gmail.com>
2024-03-20 14:23:27 +0200

    HomeWork

---
+++ HomeWork4.4/HomeWork4.4/Models/Breed.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeWork4._4.Models
+{
+    public class Breed
+    {
+        public int Id { get; set; }
+        public int BreedId { get; set; } 
+        public string BreedName { get; set; } = null!;
+    }
+}
---
+++ HomeWork4.4/HomeWork4.4/Models/Pet.cs
@@ -0,0 +1,15 @@
+﻿namespace HomeWork4._4.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int? CategoryId { get; set; }
+        public int BreedId { get; set; }
+        public float Age { get; set; }
+        public int LocationId { get; set; }
+        public string ImageUrl { get; set; } = null!;
+        public string Description { get; set; } = null!;
+
+    }
+}
