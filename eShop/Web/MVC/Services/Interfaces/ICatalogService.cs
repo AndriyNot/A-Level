2e4e14c0a4fca5563f6243ad0ecefde153b1145f
AndriyFartukh <fartuhandriy2003@gmail.com>
2024-05-24 23:53:51 +0300

    HomeWork

---
+++ eShop/Web/MVC/Services/Interfaces/ICatalogService.cs
@@ -0,0 +1,10 @@
+﻿using MVC.ViewModels;
+
+namespace MVC.Services.Interfaces;
+
+public interface ICatalogService
+{
+    Task<Catalogs> GetCatalogItems(int page, int take, int? brand, int? type);
+    Task<IEnumerable<CatalogBrand>> GetBrands();
+    Task<IEnumerable<SelectListItem>> GetTypes();
+}
